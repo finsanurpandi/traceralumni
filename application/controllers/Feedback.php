6c0a74f33f5490d8429eaf41d196fbc15f593667
finsanurpandi <finsaworks@gmail.com>
2018-11-06 13:00:23 +0700

    fixing model again...x

--- application/controllers/Feedback.php
+++ application/controllers/Feedback.php
@@ -5,7 +5,7 @@ class Feedback extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('m_feedback');
+        $this->load->model('m_tracer');
 
         date_default_timezone_set("Asia/Bangkok");
     }
